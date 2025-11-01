2d7b4e4ff2084d5580b7912a611fbd629c8069f9
omotayo olukale <omotayo.olukale@gmail.com>
2025-11-01 03:44:21 +0100

    Added pixel

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next'
 import { GeistSans } from 'geist/font/sans'
 import { GeistMono } from 'geist/font/mono'
+import Script from 'next/script'
 import './globals.css'
 
 export const metadata: Metadata = {
@@ -23,8 +24,30 @@ html {
   --font-mono: ${GeistMono.variable};
 }
         `}</style>
+        {/* Meta Pixel Code */}
+        <Script
+          id="fb-pixel-script"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              !function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}(window, document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init', '24236207556053950');
+              fbq('track', 'PageView');
+            `,
+          }}
+        />
       </head>
-      <body>{children}</body>
+      <body>
+        <noscript><img height="1" width="1" style={{display: 'none'}} src="https://www.facebook.com/tr?id=24236207556053950&ev=PageView&noscript=1" /></noscript>
+        {children}
+      </body>
     </html>
   )
 }
